e5f48af91dc142f62f7b1ed687d900bef58e4ad0
awaisdeveloper <getawaisdeveloper@gmail.com>
2019-12-31 10:04:05 +0500

    change https url

--- client/src/Services/HttpUtils.js
+++ client/src/Services/HttpUtils.js
@@ -1,8 +1,8 @@
 import { AsyncStorage } from '@callstack/async-storage'
 
-const BASE_URL = 'http://localhost:8000/api';
+// const BASE_URL = 'http://localhost:8000/api';
 //  const BASE_URL = 'https://pakjazba.com/api';
- //const BASE_URL = 'https://pure-hollows-17968.herokuapp.com/api';
+ const BASE_URL = 'https://artisana.herokuapp.com//api';
 //  const BASE_URL = 'https://getfit-server.herokuapp.com';
 
 
